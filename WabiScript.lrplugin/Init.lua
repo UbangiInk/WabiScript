e0cf9fdfffd9aa713fe628040a88fc2f8fbbf4dc
Ubangi <brx@brockporter.com>
2025-06-29 10:18:16 -0600

    Create Init.lua

---
+++ WabiScript.lrplugin/Init.lua
@@ -0,0 +1,5 @@
+-- init.lua
+local LrDialogs = import("LrDialogs")
+local caption = require("wabiCaption")
+
+LrDialogs.message("WabiScript loaded", "The scroll is blank and the ink is fresh.", "info")
